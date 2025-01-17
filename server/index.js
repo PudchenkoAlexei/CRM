87a399181411ee6148abdd5af629090bb6431186
Alex Pudchenko <leshapudchenko@gmail.com>
2025-01-17 18:31:58 +0200

    fix index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import helmet from 'helmet';
+import mongoose from 'mongoose';
 import morgan from 'morgan';
 
 import customerRoute from './routes/customer.js';
@@ -13,10 +14,30 @@ dotenv.config();
 
 const PORT = process.env.PORT || 7700;
 
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log('Connected to MongoDB.');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1); // Завершує процес з кодом помилки, якщо не вдалося підключитися
+  }
+};
+
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected!');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+// middlewares
 app.use(cookieParser());
 app.use(express.json());
 app.use(helmet());
 
+// CORS configuration
 app.use(
   cors({
     origin: 'http://localhost:3000',
@@ -26,24 +47,12 @@ app.use(
 
 app.use(morgan('common'));
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
-});
-
+// Routes
 app.use('/api/users', userRoute);
 app.use('/api/customers', customerRoute);
 
-app.use((err, req, res) => {
-  const errorStatus = err.status || 500;
-  const errorMessage = err.message || 'Something went wrong!';
-  return res.status(errorStatus).json({
-    success: false,
-    status: errorStatus,
-    message: errorMessage,
-    stack: process.env.NODE_ENV === 'development' ? err.stack : {},
-  });
-});
-
+// Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
+  connect();
 });
